a23fb87856595fb0c44c4915d75c59c1a9910db8
Mathias-hub <59036030+Mathias-hub@users.noreply.github.com>
2020-01-09 15:21:52 +0100

    Lots Of changes

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,23 +1,17 @@
-import numpy as np
 from pandas import DataFrame
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
 
 ### DEFINE DATA ###
-Data1 = {'Country': ['US','CA','GER','UK','FR'],
-        'GDP_Per_Capita': [45000,42000,52000,49000,47000]
-       }
+planets = sns.load_dataset('planets')
 
-df1 = DataFrame(Data1, columns= ['Country', 'GDP_Per_Capita'])
-df1 = df1[['Country', 'GDP_Per_Capita']].groupby('Country').sum()
 
-Data2 = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
-        'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
-       }
-  
-df2 = DataFrame(Data2,columns=['Year','Unemployment_Rate'])
-df2 = df2[['Year', 'Unemployment_Rate']].groupby('Year').sum()
+print(planets.describe())
 
-Data3 = {'Interest_Rate': [5,5.5,6,5.5,5.25,6.5,7,8,7.5,8.5],
-        'Stock_Index_Price': [1500,1520,1525,1523,1515,1540,1545,1560,1555,1565]
-       }
-  
-df3 = DataFrame(Data3,columns=['Interest_Rate','Stock_Index_Price'])
+sns.lineplot(x = 'year', y = 'number', data = planets)
+
+
+
+plt.show()
--- GUI_MainScript.py
+++ GUI_MainScript.py
@@ -7,22 +7,32 @@ import seaborn as sns
 ### DEFINE DATA ###
 planets = sns.load_dataset('planets')
 
+###### MAKE THE GUI ######
 
-### MAKE THE GUI ###
+### DEFINE MAIN GUI CANVAS WINDOW ###
 root= tk.Tk()   
+root.title("Data Analysis Tool")
+root.geometry('1000x500')
 
 ### Add Seaborn plot
-figure1 = plt.Figure(figsize=(6,4), dpi=150)
+figure1 = plt.Figure(figsize=(6,4), dpi=50)
 ax1 = figure1.add_subplot(111)
 bar1 = FigureCanvasTkAgg(figure1, root)
-bar1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-sns.catplot("year", data=planets, aspect=2,
+bar1.get_tk_widget().pack(fill=tk.X)
+g = sns.catplot("year", data=planets, aspect=2,
                        kind="count", color='steelblue',ax=ax1)
-
-# Set the step size of the x-axis
-#ax1.set_xticks(ax1.get_xticks()[::12])
+#g.set_xticklabels(step=5)
 ax1.set_title('An Awesome Bar Plot')
 
+### Add Some info stuff
+figure2 = plt.Figure(figsize=(6,4), dpi=100)
+ax2 = figure2.add_subplot(111)
+bar2 = FigureCanvasTkAgg(figure2, root)
+bar2.get_tk_widget().pack(fill=tk.X)
+planets.plot(kind='line', legend=True, ax=ax2)
 
+ax1.set_title('An Awesome Bar Plot')
+
+#g.set_ylabels('Number of Planets Discovered')
 
 root.mainloop()
